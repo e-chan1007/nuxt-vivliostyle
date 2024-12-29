bfa277e8b1f04339a398c9c1af9293e839a311bc
いーちゃん(e_chan1007) <echan041007@gmail.com>
2024-12-29 23:43:47 +0900

    fix(viewer): build was failed

--- build-viewer.mjs
+++ build-viewer.mjs
@@ -13,22 +13,36 @@ try {
 
 const { dir } = await downloadTemplate("gh:vivliostyle/vivliostyle.js#master", {
   force: true,
-  forceClean: true,
-  install: true,
 });
 
-const execProcess = (command, ...args) => {
-  const process = spawn(command, args, { cwd: dir });
+const execProcess = (command, args, cwd = dir) => {
+  const process = spawn(command, args, { cwd });
   return new Promise((resolve) => process.on("exit", resolve));
 };
+
+await writeFile(
+  join(dir, ".yarnrc.yml"),
+  "nodeLinker: pnpm\nnmHoistingLimits: dependencies",
+  "utf-8",
+);
+await execProcess("yarn", ["install"]);
 
 const netTSPath = join(dir, "packages/core/src/vivliostyle/net.ts");
 const netTS = await readFile(netTSPath, "utf-8");
 const patch = await readFile("./vivliostyle-net.patch", "utf-8");
 await writeFile(netTSPath, applyPatch(netTS, patch), "utf-8");
 
-await execProcess("yarn", "workspace", "@vivliostyle/core", "build");
-await execProcess("yarn", "workspace", "@vivliostyle/viewer", "build");
+await execProcess(
+  "yarn",
+  ["workspace", "@vivliostyle/core", "build"],
+  import.meta.dirname,
+);
+
+await execProcess(
+  "yarn",
+  ["workspace", "@vivliostyle/viewer", "build"],
+  import.meta.dirname,
+);
 
 await rm("./viewer", { recursive: true, force: true });
 await rename(join(dir, "packages/viewer/lib"), "./viewer");
