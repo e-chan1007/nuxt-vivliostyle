4263f209e367aa057174be36d49638e260e49c9d
いーちゃん(e_chan1007) <echan041007@gmail.com>
2024-12-31 16:10:14 +0900

    enhance: change reload method in order not to load first page every time

--- plugins/hard-reload.client.ts
+++ plugins/hard-reload.client.ts
@@ -1,10 +1,20 @@
 export default defineNuxtPlugin(() => {
   if (import.meta.client && import.meta.dev && import.meta.hot) {
-    const reload = () =>
+    const reload = () => {
+      const iframe = document.querySelector<HTMLIFrameElement>("#__nuxt > iframe")
+      if (iframe) {
+        const target = iframe.contentWindow?.location.hash.match(/src=([^&]*)/)?.[1]
+        if (target) {
+          // @ts-expect-error
+          iframe.contentWindow?.coreViewer.loadDocument(target);
+          return;
+        }
+      }
       reloadNuxtApp({
         path: window.location.pathname,
         force: true,
       });
+    }
     const socket = new WebSocket("ws://localhost:4000/ws");
     socket.addEventListener("message", (event) => {
       const data = JSON.parse(event.data);
